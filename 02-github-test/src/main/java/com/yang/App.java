cd59bbff6d8ff9e9f8aa47b398f30f527f8ba40b
joshua <joshua2010@163.com>
2019-11-27 13:50:20 +0800

    1

--- 02-github-test/src/main/java/com/yang/App.java
+++ 02-github-test/src/main/java/com/yang/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "joshua hello" );
+        System.out.println( "joshua hello world 2" );
     }
 }
